c773db4b87a53d9cfdd6f2c4a3c7b59faa836efb
Daniel Lim <danheelim@gmail.com>
2019-10-12 10:31:36 -0700

    can't get the file to read my-name.txt but everything looks correct still need to work on getting it to read it

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,6 +26,10 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    # here we just recieve the proof of work and we run with it since we validated it
+    # and until the valid proof is back to TRUE it just keeps running and adding a proof
+    while valid_proof(last_proof, proof) is False:
+        proof += random.randint(1,101)
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +44,20 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    # so we first recieve the proof to validate by a matching hash, it exitsts, or key
+    # then we have to encode it so it can be converted to a hexidecimal
+
+    guess_proof = f"{last_hash}{proof}".encode()
+
+    # then we have to check it's history or hashses we convert the inport "last_hash" into a string
+    last_hash = str(last_hash)
+
+    # still don't know what this whole line does i just copied from my hw I think it rehashes the proof and "checks it history" by compairing it's hash with the pervious hash i'm still not too sure and the convert it into a hexideciemal
+    hashed_proof = hashlib.sha256(guess_proof).hexdigest()
+
+    # then we return the valid proof with the number of hashse are equal to the proof which returns a TRUE or FALSE and in this case SHOULD be FALSE
+    return hashed_proof[:6] == last_hash[-6:]
+    # pass
 
 
 if __name__ == '__main__':
@@ -58,7 +75,7 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
-    if id == 'NONAME\n':
+    if id == 'danHEElim\n':
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
     # Run forever until interrupted
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -21,16 +21,23 @@ def reconstruct_trip(tickets, length):
     """
 
     for ticket in tickets:
-        # here we are inserting a hash per the length of the tickets so here forexample we have 5 tickets and so we insert the length of the ticket list, source ,and destination
+        # here we are inserting a hash per the length of the tickets so here for example we have 5 tickets and so we insert the length of the ticket list, source ,and destination
+
+        # we are starting the sequence so to speak so we can work with changing it around. 
        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    # here if find the location of 
+    # here if find the location of the first airport which had no previous airport to fly from which will be our head so in the test case it will be ticket 1 PDX or in test2 LAX
+    # so now we have our starting point and it's as simple as linking everything together since every ticket has a final destination
     location = hash_table_retrieve(hashtable, "NONE")
 
+    # so we want to loop as long as the length of the list of tickets.
     for i in range(length):
+        # the list above labled "route" has a list of a bunch of NONE's so we change FIRST index with the current location which is our head
         route[i] = location
+        # then we retrieve the next location by running "hash_table_retrieve" with the hashtable and the current "location" as the previous airport it arrived from and replacing the current location vairable with the new found destination
         location = hash_table_retrieve(hashtable, location)
 
+    # here we print because the READ ME said to
     print(route)
-
+    # and now we return it
     return route
