cae033a3a543d56a24458a8c400784e0130b932e
Daniel Lim <danheelim@gmail.com>
2019-10-12 10:04:52 -0700

    finished ex2 on HT

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
-    route = [None] * length
+    hashtable = HashTable(length) # 5
+    route = [None] * length #[None,None,None,None,None,]
 
     """
     YOUR CODE HERE
     """
+
+    for ticket in tickets:
+        # here we are inserting a hash per the length of the tickets so here forexample we have 5 tickets and so we insert the length of the ticket list, source ,and destination
+       hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    # here if find the location of 
+    location = hash_table_retrieve(hashtable, "NONE")
+
+    for i in range(length):
+        route[i] = location
+        location = hash_table_retrieve(hashtable, location)
+
+    print(route)
 
     return route
